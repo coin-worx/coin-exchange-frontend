235c6d2760af9a99b104f32ae63363db2b987bef
vladimir <baxxabit@gmail.com>
2014-04-24 12:54:40 +0300

    Removed comments.

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -61,22 +61,10 @@ grails.project.dependency.resolution = {
         compile ':cache:1.1.1'
 
         // plugins needed at runtime but not for compilation
-        runtime ":hibernate:3.6.10.13" // or ":hibernate4:4.3.4"
+        runtime ":hibernate:3.6.10.13"
         runtime ":database-migration:1.4.0"
-        // runtime ":jquery:1.11.0.2"
-        // runtime ":resources:1.2.7"
-        // Uncomment these (or add new ones) to enable additional resources capabilities
-        //runtime ":zipped-resources:1.0.1"
-        //runtime ":cached-resources:1.1"
-        //runtime ":yui-minify-resources:0.1.5"
 
-        // An alternative to the default resources plugin is the asset-pipeline plugin
         compile ":asset-pipeline:1.8.3"
 
-        // Uncomment these to enable additional asset-pipeline capabilities
-        //compile ":sass-asset-pipeline:1.5.5"
-        //compile ":less-asset-pipeline:1.5.3"
-        //compile ":coffee-asset-pipeline:1.5.0"
-        //compile ":handlebars-asset-pipeline:1.3.0.1"
     }
 }
