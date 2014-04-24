be8f6d86f18fe3ef824f6916c2cd439244df80dd
vladimir <baxxabit@gmail.com>
2014-04-24 12:46:04 +0300

    Updated work dir.

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,8 +1,8 @@
 grails.servlet.version = "3.0" // Change depending on target container compliance (2.5 or 3.0)
+grails.work.dir = "target"
 grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
-grails.project.work.dir = "target/work"
 grails.project.target.level = 1.6
 grails.project.source.level = 1.6
 //grails.project.war.file = "target/${appName}-${appVersion}.war"
