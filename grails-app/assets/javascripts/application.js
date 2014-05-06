773d8331807ef6c80f1da4155b9e1d132d87355c
vladimir <baxxabit@gmail.com>
2014-05-07 01:09:08 +0300

    (COIN-205) Added angularjs, jquery. Created manifest file for javascript.

---
+++ grails-app/assets/javascripts/application.js
@@ -0,0 +1,6 @@
+//= require jquery/dist/jquery
+//= require angular/angular
+//= require_tree views
+//= require_self
+
+console.log("manifest load complete");
--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -57,26 +57,17 @@ grails.project.dependency.resolution = {
         build ":tomcat:7.0.52.1"
 
         // plugins for the compile step
-        compile ":scaffolding:2.0.2"
-        compile ':cache:1.1.1'
+        compile ':cache:1.1.5',
+                ":scaffolding:2.0.2",
+                ":asset-pipeline:1.8.7"
 
         // plugins needed at runtime but not for compilation
-        runtime ":hibernate:3.6.10.9" // or ":hibernate4:4.3.4"
-        runtime ":database-migration:1.4.0"
-        // runtime ":jquery:1.11.0.2"
-        // runtime ":resources:1.2.7"
-        // Uncomment these (or add new ones) to enable additional resources capabilities
-        //runtime ":zipped-resources:1.0.1"
-        //runtime ":cached-resources:1.1"
-        //runtime ":yui-minify-resources:0.1.5"
-
-        // An alternative to the default resources plugin is the asset-pipeline plugin
-        compile ":asset-pipeline:1.8.3"
+        runtime ":hibernate:3.6.10.9",
+                ":database-migration:1.4.0"
 
         // Uncomment these to enable additional asset-pipeline capabilities
         //compile ":sass-asset-pipeline:1.5.5"
         //compile ":less-asset-pipeline:1.5.3"
-        //compile ":coffee-asset-pipeline:1.5.0"
         //compile ":handlebars-asset-pipeline:1.3.0.1"
     }
 }
