d0db613d544e2fb5608cda873277aa7ebf58482c
vladimir <baxxabit@gmail.com>
2014-05-22 00:08:03 +0300

    (COIN-206) Updated UrlMapping, request via 'api/query'.

--- grails-app/conf/UrlMappings.groovy
+++ grails-app/conf/UrlMappings.groovy
@@ -1,13 +1,15 @@
 class UrlMappings {
 
-	static mappings = {
-        "/$controller/$action?/$id?(.$format)?"{
+    static mappings = {
+        "/$controller/$action?/$id?(.$format)?" {
             constraints {
                 // apply constraints here
             }
         }
 
-        "/"(view:"/index")
-        "500"(view:'/error')
-	}
+        "/api/$action"(controller: 'interaction')
+
+        "/"(view: "/index")
+        "500"(view: '/error')
+    }
 }
