b617104dab0411d727790a62cf774c3d7b76aa9e
waqas_syed <waqas.syed@hotmail.com>
2014-06-23 12:54:16 +0500

    Depth Chart minor bug fix

--- grails-app/assets/javascripts/angular-source/components/marketData/orderbook/controllers/depthController.js
+++ grails-app/assets/javascripts/angular-source/components/marketData/orderbook/controllers/depthController.js
@@ -265,8 +265,10 @@ angular.module('marketData.orderBook').controller('DepthController', [
                     formatter: function() {
                         var s = [];
                         $.each(this.points, function(i, point) {
-                            s.push('<span style="color:#D31B22;font-weight:bold;">'+ 'Cm. Volume' +' : '+
-                                point.y + '<br/>' + 'Price' + ' : ' + point.x + '<span>');
+                            if(point.x != null && point.x != undefined && point.y != null && point.y != undefined){
+                                s.push('<span style="color:#D31B22;font-weight:bold;">'+ 'Cm. Volume' +' : '+
+                                    point.y + '<br/>' + 'Price' + ' : ' + point.x + '<span>');
+                            }
                         });
                         return s.join(' <br /> ');
                     },
