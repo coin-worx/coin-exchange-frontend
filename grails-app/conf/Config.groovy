95585364ad93469c87d266d60eb128e0bb6ca073
waqas_syed <waqas.syed@hotmail.com>
2014-09-03 18:20:33 +0500

    Url change

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -162,10 +162,10 @@ grails.assets.includes = []
 }
 
 // ToDo: Restore to the address and prefix when hosting the back-end. Currently localhost being used for testing purposes only
-blancrock.backend.baseUrl = 'http://localhost:51780'
-blancrock.backend.login.prefix = '/v1'
-//blancrock.backend.baseUrl = 'http://rockblanc.cloudapp.net'
-//blancrock.backend.login.prefix = '/test/v1'
+//blancrock.backend.baseUrl = 'http://localhost:51780'
+//blancrock.backend.login.prefix = '/v1'
+blancrock.backend.baseUrl = 'http://localhost'
+blancrock.backend.login.prefix = '/test/v1'
 blancrock.backend.loginPath = blancrock.backend.login.prefix + '/admin/login'
 blancrock.backend.logoutPath = blancrock.backend.login.prefix + '/private/admin/logout'
 blancrock.auth.nounce = ''
